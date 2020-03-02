fd5fdcb0d0f4e91cce8abdad39d16c9f688c215c
Landung Setiawan <landungs@uw.edu>
2020-03-02 14:15:32 -0800

    Add improvements

--- cadai/main.py
+++ cadai/main.py
@@ -1,19 +1,22 @@
 import logging
 import os
 
-from fastapi import FastAPI
+from dask.distributed import Client
+
+from fastapi import FastAPI, BackgroundTasks
 from starlette.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
 from .api.main import api_router
 from .core.config import CORS_ORIGINS, API_TITLE, API_DESCRIPTION, BASE_PATH
-from .scripts import load_datasets
+from .scripts import LoadDatasets
 
 logger = logging.getLogger(__name__)
 logging.root.setLevel(level=logging.INFO)
 
 app = FastAPI(title=API_TITLE, description=API_DESCRIPTION)
+client = Client()
 
 app.state.static = StaticFiles(directory=os.path.join(BASE_PATH, "static"))
 app.state.templates = Jinja2Templates(directory=os.path.join(BASE_PATH, "templates"))
@@ -32,4 +35,4 @@ app.mount("/static", app.state.static, name="static")
 
 @app.on_event("startup")
 async def startup_event():
-    load_datasets(app)
+    LoadDatasets(app)
--- cadai/scripts.py
+++ cadai/scripts.py
@@ -1,3 +1,5 @@
+import concurrent.futures
+import threading
 import logging
 from .store import DATASETS_STORE
 from .models import XRDataset
@@ -58,16 +60,51 @@ URLS = [
 ]
 
 
-def load_datasets(app):
-    logger.info("Datasets loading...")
-    for url in URLS:
+class LoadDatasets:
+    """ Threading example class
+    The run() method will be started and it will run in the background
+    until the application exits.
+    """
+
+    def __init__(self, app):
+        """ Constructor
+        :type interval: int
+        :param interval: Check interval, in seconds
+        """
+        self._app = app
+        self._urls = URLS
+        self._in_progress = True
+
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True  # Daemonize thread
+        thread.start()  # Start the execution
+
+    def run(self):
+        """ Method that runs forever """
+        while self._in_progress:
+            logger.info("Datasets loading...")
+            with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+                # Start the load operations and mark each future with its URL
+                future_to_url = {
+                    executor.submit(self.load_dataset, url): url
+                    for url in self._urls
+                }
+                for future in concurrent.futures.as_completed(future_to_url):
+                    url = future_to_url[future]
+                    data = future.result()
+                    if data:
+                        logger.info(f"{url} loaded.")
+
+            self._in_progress = False
+            logger.info("Datasets loaded.")
+
+    def load_dataset(self, url):
         try:
             logger.info(f"Loading: {url}")
             xrd = XRDataset(url, mounted=True)
             xrd.set_ds()
             DATASETS_STORE[xrd.dataset_id] = xrd
-            app.mount(f"/{xrd.dataset_id}", DATASETS_STORE[xrd.dataset_id].app)
+            self._app.mount(f"/{xrd.dataset_id}", DATASETS_STORE[xrd.dataset_id].app)
         except Exception as e:
             logger.warning(e)
             logger.info(f"Skipping: {url}")
-    logger.info("Datasets loaded.")
