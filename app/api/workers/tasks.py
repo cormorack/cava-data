e0addfa8703c57f69352f15bd29bb4f059e984f1
Landung "Don" Setiawan <landungs@uw.edu>
2021-05-13 16:18:08 -0700

    Fix fetching bug (#8)

--- app/api/workers/tasks.py
+++ app/api/workers/tasks.py
@@ -22,8 +22,8 @@ def perform_fetch_task(self, data_request):
         "type": job_type,
         "msg": "Data retrieval started.",
     }
-    start_dt = parser.parse(data_request['start_dt'])
-    end_dt = parser.parse(data_request['end_dt'])
+    start_dt = data_request['start_dt']
+    end_dt = data_request['end_dt']
     result = fetch(
         self,
         request_params,
