ec79651aae86114d6bcbe2ae181d2aebd42d6f20
lsetiawan <landungs@uw.edu>
2021-06-15 11:57:46 -0700

    Use interpolation to match time and add dynamic cluster

--- app/api/workers/data_fetcher.py
+++ app/api/workers/data_fetcher.py
@@ -5,6 +5,9 @@ import zarr
 import fsspec
 import datashader
 import hvplot.xarray  # noqa
+import pandas as pd
+from dask_kubernetes import KubeCluster, make_pod_spec
+from dask.distributed import Client
 from .models import OOIDataset
 
 logger = logging.getLogger(__name__)
@@ -26,35 +29,16 @@ def setup_params(axis_params):
     return parameters
 
 
-def _merge_datasets(data_list, axis_params):
+def _merge_datasets(data_list: dict, start_dt: str, end_dt: str) -> xr.Dataset:
     """Merges all dataset in data_list together into a single one"""
     # Merge data_list
-    dslist = []
-
     # --- This way of merging is for simple data only! ---
-    sorted_keys = sorted(
-        data_list, key=lambda x: len(data_list[x].time), reverse=True
-    )
-
-    highest = sorted_keys[0]
-    for idx, k in enumerate(sorted_keys):
-        if idx == 0:
-            res = data_list[k]
-        else:
-            res = data_list[k].reindex_like(
-                data_list[highest],
-                method="nearest",
-                tolerance="1s",
-            )
-        dslist.append(res)
+    # Align time based on request start and end datetime string
+    new_time = pd.date_range(start_dt, end_dt, freq="1s")
+    dslist = [_interp_ds(ds, new_time) for k, ds in data_list.items()]
     # --- Done one way of merging ---
 
     merged = xr.merge(dslist, combine_attrs="no_conflicts").unify_chunks()
-
-    # Swapping dimensions for plotting to work if time is not
-    # an axis selection
-    if axis_params["x"] != "time":
-        merged = merged.swap_dims({"time": axis_params['x']})
 
     return merged
 
@@ -145,6 +129,101 @@ def fetch_zarr(zarr_url, storage_options={'anon': True}):
     return zg, dimensions, variable_arrays
 
 
+def _interp_ds(
+    ds: xr.Dataset, new_time: pd.DatetimeIndex, method: str = 'nearest'
+) -> xr.Dataset:
+    return ds.interp(time=new_time).interpolate_na(
+        dim='time', method=method, fill_value="extrapolate"
+    )
+
+
+def _clean_pod_spec(pod_spec):
+    """Cleans pod specification for k8s"""
+    # Set default API Version and kind
+    setattr(pod_spec, 'api_version', 'v1')
+    setattr(pod_spec, 'kind', 'Pod')
+    # Cleans memory values for resources
+    for c in pod_spec.spec.containers:
+        for attr in c.resources.attribute_map.values():
+            attr_val = getattr(c.resources, attr)
+            val = attr_val['memory']
+            val = val.replace("GB", "G")
+            attr_val.update({'memory': val})
+            setattr(c.resources, attr, attr_val)
+    return pod_spec
+
+
+def determine_workers(
+    max_mem_size: int,
+    memory_limit: int = 16,
+    cpu_limit: int = 2,
+    image_repo: str = 'cormorack',
+    image_name: str = 'cava-data',
+    image_tag: str = '20210610',
+) -> dict:
+    """
+    Determine dask worker spec and cluster size,
+    based on total requested data size
+
+    Parameters
+    ----------
+    max_mem_size: int
+        Max memory requirement for the amount of data requested
+    memory_limit: int
+        Memory limit for the dask worker, as well max machine memory
+    cpu_limit: int
+        CPU limit for the dask worker
+    image_repo: str
+        Docker image repository for dask worker
+    image_name:
+        Docker image name for dask worker
+    image_tag:
+        Docker image tag for dask worker
+
+    Returns
+    -------
+    dict
+        Dictionary containing the pod_spec,
+        and min, max for number of workers
+
+    """
+    max_workers = int(np.ceil(max_mem_size / memory_limit))
+    min_workers = int(np.ceil(max_workers / 10))
+    image = f"{image_repo}/{image_name}:{image_tag}"
+
+    # Determine the memory and cpu request sizes
+    k8s_mem = memory_limit / 2
+    k8s_cpu = cpu_limit / 2
+    if max_mem_size < memory_limit:
+        k8s_mem = max_mem_size
+        k8s_cpu = k8s_cpu / 2
+
+    pod_spec = make_pod_spec(
+        image=image,
+        labels={'app.kubernetes.io/component': 'cava-dask'},
+        memory_limit=f'{memory_limit}GB',
+        memory_request=f'{k8s_mem}GB',
+        cpu_limit=str(cpu_limit),
+        cpu_request=str(k8s_cpu),
+        extra_pod_config={
+            'nodeSelector': {'kops.k8s.io/instancegroup': 'compute'},
+            'restartPolicy': 'Never',
+        },
+        extra_container_config={
+            'imagePullPolicy': 'IfNotPresent',
+            'name': 'cava-dask',
+        },
+        threads_per_worker=2,
+    )
+
+    cleaned_spec = _clean_pod_spec(pod_spec)
+    return {
+        'min_workers': min_workers,
+        'max_workers': max_workers,
+        'pod_spec': cleaned_spec,
+    }
+
+
 def fetch(
     self,
     request_params,
@@ -154,6 +233,7 @@ def fetch(
     download,
     download_format,
     status_dict,
+    attrs_check: bool = False,
 ):
     self.update_state(
         state="PROGRESS",
@@ -163,6 +243,29 @@ def fetch(
     if "time" not in parameters:
         parameters.append("time")
 
+    # Set up dask computation cluster
+    status_dict.update({"msg": "Setting up computation cluster..."})
+    self.update_state(state="PROGRESS", meta=status_dict)
+    ds_list = {}
+    for dataset_id in request_params:
+        ooids = OOIDataset(dataset_id)[parameters]
+        ds_list[dataset_id] = {
+            'dataset': ooids,
+            'total_size': ooids._total_size,
+        }
+    max_mem_size = (
+        np.sum([v['total_size'] for v in ds_list.values()]) / 1024 ** 3
+    )
+    dask_spec = determine_workers(max_mem_size)
+
+    cluster = KubeCluster(
+        dask_spec['pod_spec'], n_workers=dask_spec['min_workers']
+    )
+    cluster.adapt(
+        minimum=dask_spec['min_workers'], maximum=dask_spec['max_workers']
+    )
+    client = Client(cluster)  # noqa
+
     # TODO: Need to add other parameters for multidimensional
     # need a check for nutnr,pco2,ph,optaa add int_ctd_pressure
     # parameters.append("int_ctd_pressure")
@@ -170,42 +273,68 @@ def fetch(
     # for spikr
     # parameters.append("spectra")
 
-    status_dict.update({"msg": "Retrieving data from store..."})
+    status_dict.update({"msg": "Retrieving data from zarr store ..."})
     self.update_state(state="PROGRESS", meta=status_dict)
-    data_list = {}
-    for dataset_id in request_params:
-        ooids = OOIDataset(dataset_id)[parameters].sel(time=(start_dt, end_dt))
-        data_list[dataset_id] = ooids.dataset
+    data_list = {
+        k: v['dataset']
+        .sel(time=(start_dt, end_dt), use_attrs=attrs_check)
+        .dataset
+        for k, v in ds_list.items()
+    }
 
-    status_dict.update({"msg": "Reindexing and merging datasets..."})
+    status_dict.update({"msg": "Validating datasets..."})
     self.update_state(state="PROGRESS", meta=status_dict)
-    merged = _merge_datasets(data_list, axis_params)
-    data_count = len(merged.time)
-
-    if data_count == 0:
+    if any(True for _, v in data_list.items() if v is None):
+        # Checks if data_list is None
+        status_dict.update(
+            {"msg": "One of the dataset does not contain data."}
+        )
+        self.update_state(state="PROGRESS", meta=status_dict)
         result = None
     else:
-        # Shading process
-        final_dct, shaded, color_column = _plot_merged_dataset(
-            merged, axis_params
-        )
-        x = final_dct.get(axis_params['x'], [])
-        y = final_dct.get(axis_params['y'], [])
-        z = []
-        if axis_params['z']:
-            z = final_dct.get(axis_params['z'], np.array([]))
-        elif shaded:
-            z = final_dct.get(color_column, np.array([]))
+        status_dict.update({"msg": "All datasets contain data..."})
+        self.update_state(state="PROGRESS", meta=status_dict)
+        if len(data_list.keys()) > 1:
+            status_dict.update(
+                {"msg": "Interpolating and merging datasets..."}
+            )
+            self.update_state(state="PROGRESS", meta=status_dict)
+            merged = _merge_datasets(data_list, axis_params, start_dt, end_dt)
+        else:
+            merged = next(ds for _, ds in data_list.items())
 
-        result = (
-            {
-                "x": x,
-                "y": y,
-                "z": z,
-                "count": data_count,
-                "shaded": shaded,
-            },
-        )
-    logger.info("Result done.")
+        data_count = len(merged.time)
+
+        if data_count == 0:
+            status_dict.update(
+                {"msg": "Merged dataset does not contain data."}
+            )
+            self.update_state(state="PROGRESS", meta=status_dict)
+            result = None
+        else:
+            status_dict.update({"msg": "Plotting merged datasets..."})
+            self.update_state(state="PROGRESS", meta=status_dict)
+            # Shading process
+            final_dct, shaded, color_column = _plot_merged_dataset(
+                merged, axis_params
+            )
+            x = final_dct.get(axis_params['x'], [])
+            y = final_dct.get(axis_params['y'], [])
+            z = []
+            if axis_params['z']:
+                z = final_dct.get(axis_params['z'], np.array([]))
+            elif shaded:
+                z = final_dct.get(color_column, np.array([]))
+
+            result = (
+                {
+                    "x": x,
+                    "y": y,
+                    "z": z,
+                    "count": data_count,
+                    "shaded": shaded,
+                },
+            )
+        logger.info("Result done.")
     # ================ End Compute results ========================
     return result
