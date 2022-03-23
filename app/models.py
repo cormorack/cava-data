95f4d647dca41bf0b47ef2d7f3c43b0ec844ff1c
lsetiawan <landungs@uw.edu>
2022-03-23 09:55:16 -0700

    Clean up xpublish

--- app/models.py
+++ app/models.py
@@ -1,13 +1,8 @@
-import os
 import json
 import hashlib
 
-import xarray as xr
-import xpublish  # noqa
-
 from pydantic import BaseModel, PrivateAttr
 from typing import Optional
-from .core.config import settings
 
 
 class DataRequest(BaseModel):
@@ -35,70 +30,3 @@ class DataRequest(BaseModel):
 
 class CancelConfig(BaseModel):
     signal: Optional[str] = 'SIGUSR1'
-
-
-class XRDataset:
-    def __init__(self, zarr_url, mounted=False):
-        self._zarr_url = zarr_url
-        self._mounted = mounted
-        self._dataset_id = ""
-        self._ds = None
-        self._fs = settings.FILE_SYSTEMS['aws_s3']
-
-        self.setup()
-
-    @property
-    def zarr_url(self):
-        return self._zarr_url
-
-    @property
-    def dataset_id(self):
-        return self._dataset_id
-
-    @property
-    def is_mountable(self):
-        return self._mounted
-
-    @property
-    def router(self):
-        if isinstance(self._ds, xr.Dataset):
-            return self._ds.rest.app.router
-
-    @property
-    def app(self):
-        if isinstance(self._ds, xr.Dataset):
-            if self._mounted:
-                self._ds.rest(
-                    app_kws=dict(
-                        title=self._dataset_id,
-                        openapi_prefix=f"/{self._dataset_id}",
-                    )
-                )
-            return self._ds.rest.app
-
-    @property
-    def dataset(self):
-        return self._ds
-
-    @property
-    def prefix(self):
-        return f"/{self._dataset_id}"
-
-    def __repr__(self):
-        return f"<XRDataset: {self._dataset_id}>"
-
-    def setup(self):
-        self._dataset_id = os.path.basename(self._zarr_url)
-        if ".zarr" in self._dataset_id:
-            self._dataset_id = self._dataset_id.replace(".zarr", "")
-
-    def set_ds(self, ds=None):
-        if isinstance(ds, xr.Dataset):
-            self._ds = ds
-        elif ds is None:
-            self._ds = xr.open_zarr(
-                self._fs.get_mapper(self._zarr_url), consolidated=True
-            )
-            for k, v in self._ds.variables.items():
-                if 'chunks' in v.encoding:
-                    del v.encoding['chunks']
